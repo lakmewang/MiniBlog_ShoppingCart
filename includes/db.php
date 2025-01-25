d9bc67ee83acf6d71c1f33876983b56ece56d124
Lakmewan Gambheera Arachchi <155669037+lakmewang@users.noreply.github.com>
2025-01-25 12:44:47 +0530

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'mini_blog_cart';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
